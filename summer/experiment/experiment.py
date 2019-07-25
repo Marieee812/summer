483be5d96b97ba9773958b6413d17f9cec069805
Christopher Vibe <chrismvi@stud.ntnu.no>
2019-07-25 16:58:20 +0200

    exp: filter = 5

--- summer/experiment/experiment.py
+++ summer/experiment/experiment.py
@@ -38,7 +38,7 @@ class Experiment(ExperimentBase):
 
         self.depth = 6
         self.model = UNet(
-            in_channels=1, n_classes=1, depth=self.depth, wf=2, padding=True, batch_norm=True, up_mode="upconv"
+            in_channels=1, n_classes=1, depth=self.depth, wf=5, padding=True, batch_norm=True, up_mode="upconv"
         )
 
         self.train_dataset = Fluo_N2DH_SIM(one=True, two=True, labeled_only=True, transform=self.train_transform)
@@ -51,7 +51,6 @@ class Experiment(ExperimentBase):
         self.batch_size = 1
         self.eval_batch_size = 1
         self.precision = torch.float
-        # self.loss_fn = torch.nn.BCEWithLogitsLoss()
         self.loss_fn = torch.nn.BCEWithLogitsLoss()
         self.optimizer_cls = torch.optim.Adam
         self.optimizer_kwargs = {"lr": 1e-5, "eps": eps_for_precision[self.precision]}
