f26cc1a538e4e356a40cde2911cc80e3c581d503
ArtoVi <training@pc-atcteach21.embl.de>
2019-07-25 17:12:15 +0200

    exp: eval=False and wf=4 second try

--- summer/experiment/experiment.py
+++ summer/experiment/experiment.py
@@ -78,14 +78,14 @@ class Experiment(ExperimentBase):
 
          img, seg = self.to_tensor(img, seg, stat)
 
-         # if self.precision == torch.half and img.get_device() == -1:
-             # meager support for cpu half tensor
-         #     img = img.to(dtype=torch.float)
-         #     img += torch.zeros_like(img).normal_(std=0.1)
-         #     img = img.to(dtype=self.precision)
-         # else:
-         #     img += torch.zeros_like(img).normal_(std=0.1)
-         #
+         if self.precision == torch.half and img.get_device() == -1:
+             meager support for cpu half tensor
+             img = img.to(dtype=torch.float)
+             img += torch.zeros_like(img).normal_(std=0.1)
+             img = img.to(dtype=self.precision)
+         else:
+             img += torch.zeros_like(img).normal_(std=0.1)
+
          return img, seg
 
     def eval_transform(
