711e431b6127ab004e20912574ebfc2769ee95d5
Mirna Baksa <mirna.baksa@fer.hr>
2019-07-25 13:34:08 +0200

    exp: up

--- summer/experiment/experiment.py
+++ summer/experiment/experiment.py
@@ -83,10 +83,10 @@ class Experiment(ExperimentBase):
         if self.precision == torch.half and img.get_device() == -1:
              # meager support for cpu half tensor
             img = img.to(dtype=torch.float)
-            img += torch.zeros_like(img).random_(from=0, to=256)
+            img += torch.zeros_like(img).normal_(std=0.1)
             img = img.to(dtype=self.precision)
         else:
-            img += torch.zeros_like(img).random_(from=0, to=256)
+            img += torch.zeros_like(img).normal_(std=0.1)
 
         return img, seg
 
